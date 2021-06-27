8396dfc457a0759606d6cfbfee9a4d30a9241f95
Dustin Brown <dustin.brown.k@gmail.com>
2021-06-27 11:13:26 -0400

    Working on logic for passing player moves

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //Tic-Tac-Toe Main Script
 
 //Left Off
-//6.23 - working on module for gameboard. 
+//6.27 - working on module for gameboard. 
 //working out logic for listeners for player moves
+// trying to pass user.mark 
 
 
 const gameBoard = (() => {
@@ -12,36 +13,69 @@ const gameBoard = (() => {
     const size = 3;
 
 
-    function displayGameBoard () {
+    function createDisplayGameBoard () {
         //creates rows
         for (let i = 0; i < size; ++i){
             let row = document.createElement("div");
             container.appendChild(row).className = "rows";
         }
+        let dataNum = 0; // to identify each individual cell use for stylings
         for (let i = 0; i < size; ++i){
             for (let j = 0; j < size; ++j){
                 let cell = document.createElement("div");
-                cell.innerText = "s";
+                cell.innerText = "-";
+                cell.setAttribute("data", dataNum);
+               console.log(cell.getAttribute("data"));
                 rows[j].appendChild(cell).className = "cells";
+                dataNum++;
             }
         }
 
     }
-    function markPlayer( playGrid){
-        let playerMark = "2";
+    function markPlayer(playGrid, player){
+        let playerMark = player;
         playGrid.innerText = playerMark;
+        console.log("hello")
 
     }
 
-    function gameGrid() {
+    function gameGrid(player) {
         let playGrid = [];
         for (let i =0; i< cells.length; ++i){
             playGrid[i] = cells[i];
-            playGrid[i].addEventListener("click", markPlayer(playGrid[i]));
+            console.log(player)
+            playGrid[i].addEventListener("click",()=> {
+                markPlayer(playGrid[i], player);
+            });
         }
     };
-    return {displayGameBoard,gameGrid};
+
+    createDisplayGameBoard();
+    return {gameGrid};
+})();
+const Player = function(name, mark){
+    this.name = name;
+    this.mark = mark;
+}
+const userChoice = (() => {
+    function getPlayer(){
+        let user;
+        const markX = document.getElementById("playerX");
+        const markO = document.getElementById("playerO");
+        markX.addEventListener("click", ()=>{
+            user = new Player("Player 1", "x");
+            return user.mark;
+        });
+        markO.addEventListener("click", () => {
+            //let user = Player("Player 1", "O");
+    
+        });
+        return user;
+    }
+
+  return {getPlayer};
 })();
 
-gameBoard.displayGameBoard();
-gameBoard.gameGrid();
+let a = userChoice.getPlayer()
+gameBoard.gameGrid(a);
+//assigns user/computer 
